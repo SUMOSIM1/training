d217ac7716afb4ed3d6e602cdb7ae65ac10753f1
wwagner4 <wwagner4@gmail.com>
2024-05-07 08:04:03 +0200

    moved controller to simrunner

--- training/simrunner.py
+++ training/simrunner.py
@@ -1,6 +1,53 @@
 from pathlib import Path
 import subprocess as sp
 
+from enum import Enum, auto
+from dataclasses_json import dataclass_json, DataClassJsonMixin
+from dataclasses import dataclass
+import json
+import UdpClient as udp
+
+class SectorName(Enum):
+    UNDEF = "undef"
+    LEFT = "left"
+    CENTER = "center"
+    RIGHT = "right"
+
+class Command(Enum):
+    START = "START"
+    STOP = "STOP"
+
+@dataclass
+class SensorDto(DataClassJsonMixin):
+    xpos: float
+    ypos: float
+    direction: float
+    opponentinsector: SectorName
+    leftdistance: float
+    frontdistance: float
+    rightdistance: float
+
+@dataclass_json
+@dataclass
+class DiffDriveValuesDto:
+    rightvelo: float
+    leftvelo: float
+
+@dataclass
+class CommandDto(DataClassJsonMixin):
+    command: Command
+
+def sendAndWait(cmd: Command, config: udp.Config) -> any:
+    j = cmd.to_json()
+    udp.sendAndWait(j, config)
+
+
+
+cmd = CommandDto(Command.START)
+config = udp.Config()
+sendAndWait(cmd, config)
+
+
 def run(path: Path):
     if not path.exists():
         raise RuntimeError(f"simpath {path} does not exist.")
