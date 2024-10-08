10d7eab9739defd3da966301b693c25449e0a8e3
wwagner4 <wwagner4@gmail.com>
2024-10-08 08:37:46 +0200

    RL mapping removed

--- training/tryout.py
+++ training/tryout.py
@@ -1,12 +1,4 @@
-import training.value_mapping as vm
 
 
 def main():
-    max_speed = 7.0
-    max_speed_index = 5
-    t1 = vm._diff_drive_tuple(max_speed, max_speed_index)
-
-    for j, (a, b) in enumerate(t1):
-        print(
-            f"           ({max_speed:.2f}, {max_speed_index}, {j}, {a:.2f}, {b:.2f}),"
-        )
+    print("## tryout")
