2aabe224e2a862659c989b74c3b33f5a83366231
wwagner4 <wwagner4@gmail.com>
2025-01-14 08:16:20 +0100

    report filter yaml

--- src/training/explore/report.py
+++ src/training/explore/report.py
@@ -1,6 +1,11 @@
 from pathlib import Path
+import yaml
+from pprint import pprint
 
 def report(name_prefix: str):
     print(f"Creating report for {name_prefix}")
-    yml_path = Path(__file__).parent.parent.parent.parent / "resources" / "report.yml"
-    print(yml_path, yml_path.exists())
+    report_path = Path(__file__).parent.parent.parent.parent / "resources" / "report.yml"
+    with report_path.open() as f:
+        reports = yaml.safe_load(f)
+        filtered_reports = [r for r in reports if r['prefix'].startswith(name_prefix) ]
+        pprint(filtered_reports)
