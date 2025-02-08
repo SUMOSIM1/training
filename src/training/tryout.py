d8b65789fed2c9ad6d8aaeecf07439b0981a7fa1
wwagner4 <wwagner4@gmail.com>
2025-02-08 23:06:09 +0100

    nonlin step function

--- src/training/tryout.py
+++ src/training/tryout.py
@@ -6,6 +6,21 @@ import matplotlib.pyplot as plt
 import training.helper as hlp
 
 
+def func2(x: float, g: float) -> float:
+    f = math.pow(10, g)
+    return math.pow(f, x - 1) + (math.pow(f, - 1) * (x - 1))
+
+
+def cont_to_discrete(
+    x: float, min_value: float, max_value: float, step_count: int, linear: float
+) -> int:
+    x1 = (x - min_value) / (max_value - min_value) 
+    y1 = func2(x1, linear)
+    d = 1.0 / step_count
+    i = int(math.floor((y1 / d)))
+    return min(max(0, i), (step_count - 1))
+
+
 def nonlin():
     work_dir = Path.home() / "tmp"
 
@@ -43,8 +58,11 @@ def main():
     n = 5
     xs = np.arange(min_value, max_value, (max_value - min_value) / (dist - 0.999999)   )
     fig, ax = plt.subplots(figsize=(15, 15))
-    ys = [hlp.cont_to_discrete(x, min_value, max_value, n) for x in xs]
-    ax.plot(xs, ys)
+    ls = [0, 1, 2, 3, 4]
+    for l in ls:
+        ys = [cont_to_discrete(x, min_value, max_value, n, l) for x in xs]
+        ax.plot(xs, ys)
     out_path = work_dir / "b.png"
     fig.savefig(out_path)
     plt.close(fig)
+    print(f"Wrote step plot to '{out_path}'")
